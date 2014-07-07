928cb16b6d2a17414537f0c9e650665df8fb4906
jamming <juanjoaguililla@gmail.com>
2014-07-08 00:21:59 +0200

    Copyright format

--- src/main/java/spark/webserver/MatcherFilter.java
+++ src/main/java/spark/webserver/MatcherFilter.java
@@ -1,18 +1,15 @@
 /*
- * Copyright 2011- Per Wendel
+ * Copyright © 2011 Per Wendel. All rights reserved.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
  *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
  */
 
 package spark.webserver;
@@ -176,6 +173,7 @@ public class MatcherFilter implements Filter {
             chain.doFilter (httpReq, httpRes);
         }
 
+        // TODO this is an instance variable take care of multi-threading!
         handled = true;
 
         // TODO Merge logs and take care of method flow to log always
