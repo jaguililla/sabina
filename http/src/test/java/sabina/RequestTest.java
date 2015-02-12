b15caca13f4c0211c1350dd42b4a494ee1e5858f
jamming <juanjoaguililla@gmail.com>
2015-02-12 22:13:58 +0100

    test(http): refactor and fix 'request' tests

--- http/src/test/java/sabina/RequestTest.java
+++ http/src/test/java/sabina/RequestTest.java
@@ -14,10 +14,12 @@
 
 package sabina;
 
+import static org.apache.http.HttpStatus.*;
 import static org.testng.Assert.assertEquals;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.security.Principal;
 import java.util.*;
@@ -34,49 +36,122 @@ public class RequestTest {
 
     RouteMatch match = new RouteMatch (null, "/hi", "/hi");
 
-//    @Test public void queryParamShouldReturnsParametersFromQueryString () {
-//        Map<String, String[]> params = new HashMap<> ();
-//        params.put ("name", new String[] { "Federico" });
-//        HttpServletRequest servletRequest = new MockedHttpServletRequest (params);
-//        Request request = new Request (match, servletRequest);
-//        String name = request.queryParams ("name");
-//        assertEquals (name, "Federico", "Invalid name in query string");
+    @Test public void queryParamShouldReturnsParametersFromQueryString () {
+        Map<String, String[]> params = new HashMap<> ();
+        params.put ("name", new String[] { "Federico" });
+        HttpServletRequest servletRequest = new MockedHttpServletRequest (params);
+        HttpServletResponse servletResponse = new MockedHttpServletResponse ();
+        Request request = new Request (match, servletRequest, servletResponse);
+        String name = request.queryParams ("name");
+        assertEquals (name, "Federico", "Invalid name in query string");
+    }
+
+    @Test public void shouldBeAbleToGetTheServletPath () {
+        HttpServletRequest servletRequest = new MockedHttpServletRequest (new HashMap<> ()) {
+            @Override public String getServletPath () {
+                return THE_SERVLET_PATH;
+            }
+        };
+        HttpServletResponse servletResponse = new MockedHttpServletResponse ();
+        Request request = new Request (match, servletRequest, servletResponse);
+        assertEquals (
+            request.servletPath (), THE_SERVLET_PATH,
+            "Should have delegated getting the servlet path");
+    }
+
+    @Test public void shouldBeAbleToGetTheContextPath () {
+        HttpServletRequest servletRequest = new MockedHttpServletRequest (new HashMap<> ()) {
+            @Override public String getContextPath () {
+                return THE_CONTEXT_PATH;
+            }
+        };
+        HttpServletResponse servletResponse = new MockedHttpServletResponse ();
+        Request request = new Request (match, servletRequest, servletResponse);
+        assertEquals (
+            request.contextPath (), THE_CONTEXT_PATH,
+            "Should have delegated getting the context path");
+    }
+
+//    @Test (expectedExceptions = IllegalArgumentException.class)
+//    public void contextWithNullRequest () {
+//        new Request (null, new Response ());
 //    }
 //
-//    @Test (enabled = false) public void queryParamShouldBeParsedAsHashMap () {
-//        Map<String, String[]> params = new HashMap<> ();
-//        params.put ("user[name]", new String[] { "Federico" });
-//        HttpServletRequest servletRequest = new MockedHttpServletRequest (params);
-//        Request request = new Request (match, servletRequest);
-//        String name = request.queryMap ("user").value ("name");
-//        assertEquals (name, "Federico", "Invalid name in query string");
+//    @Test (expectedExceptions = IllegalArgumentException.class)
+//    public void contextWithNullResponse () {
+//        new Request (new Request (), null);
 //    }
 //
-//    @Test public void shouldBeAbleToGetTheServletPath () {
-//        HttpServletRequest servletRequest = new MockedHttpServletRequest (new HashMap<> ()) {
-//            @Override public String getServletPath () {
-//                return THE_SERVLET_PATH;
-//            }
-//        };
-//        Request request = new Request (match, servletRequest);
-//        assertEquals (
-//            request.servletPath (), THE_SERVLET_PATH,
-//            "Should have delegated getting the servlet path");
+//    @Test (expectedExceptions = IllegalArgumentException.class)
+//    public void contextWithNullRequestAndResponse () {
+//        new Request (null, null);
 //    }
 //
-//    @Test public void shouldBeAbleToGetTheContextPath () {
-//        HttpServletRequest servletRequest = new MockedHttpServletRequest (new HashMap<> ()) {
-//            @Override public String getContextPath () {
-//                return THE_CONTEXT_PATH;
-//            }
-//        };
-//        Request request = new Request (match, servletRequest);
-//        assertEquals (
-//            request.contextPath (), THE_CONTEXT_PATH,
-//            "Should have delegated getting the context path");
+//    @Test public void validContext () {
+//        Request exchange = new Request (new Request (), new Response ());
+//        assertTrue (exchange.request != null);
+//        assertTrue (exchange.response != null);
 //    }
 
-    public static class MockedHttpServletRequest implements HttpServletRequest {
+    @Test (expectedExceptions = HaltException.class)
+    public void halt () {
+        try {
+            Request exchange = createRequest ();
+            exchange.halt ();
+        }
+        catch (HaltException he) {
+            assertEquals (he.statusCode, SC_OK);
+            assertEquals (he.body, null);
+            throw he;
+        }
+    }
+
+    @Test (expectedExceptions = HaltException.class)
+    public void haltStatus () {
+        try {
+            Request exchange = createRequest ();
+            exchange.halt (SC_ACCEPTED);
+        }
+        catch (HaltException he) {
+            assertEquals (he.statusCode, SC_ACCEPTED);
+            assertEquals (he.body, null);
+            throw he;
+        }
+    }
+
+    @Test (expectedExceptions = IllegalArgumentException.class)
+    public void haltInvalidStatus () {
+        Request exchange = createRequest ();
+        exchange.halt (99);
+    }
+
+    @Test (expectedExceptions = HaltException.class)
+    public void haltBody () {
+        try {
+            Request exchange = createRequest ();
+            exchange.halt ("body");
+        }
+        catch (HaltException he) {
+            assertEquals (he.statusCode, SC_OK);
+            assertEquals (he.body, "body");
+            throw he;
+        }
+    }
+
+    @Test (expectedExceptions = HaltException.class)
+    public void haltStatusAndBody () {
+        try {
+            Request exchange = createRequest ();
+            exchange.halt (SC_ACCEPTED, "body");
+        }
+        catch (HaltException he) {
+            assertEquals (he.statusCode, SC_ACCEPTED);
+            assertEquals (he.body, "body");
+            throw he;
+        }
+    }
+
+    private static class MockedHttpServletRequest implements HttpServletRequest {
         private Map<String, String[]> params;
 
         public MockedHttpServletRequest (Map<String, String[]> params) {
@@ -175,5 +250,52 @@ public class RequestTest {
             throws IOException, ServletException { return null; }
 
         @Override public long getContentLengthLong () { return 0; }
+    }
+
+    private static class MockedHttpServletResponse implements HttpServletResponse {
+        @Override public void addCookie (Cookie cookie) {}
+        @Override public boolean containsHeader (String name) { return false; }
+        @Override public String encodeURL (String url) { return null; }
+        @Override public String encodeRedirectURL (String url) { return null; }
+        @Deprecated @Override public String encodeUrl (String url) { return null; }
+        @Deprecated @Override public String encodeRedirectUrl (String url) { return null; }
+        @Override public void sendError (int sc, String msg) throws IOException {}
+        @Override public void sendError (int sc) throws IOException {}
+        @Override public void sendRedirect (String location) throws IOException {}
+        @Override public void setDateHeader (String name, long date) {}
+        @Override public void addDateHeader (String name, long date) {}
+        @Override public void setHeader (String name, String value) {}
+        @Override public void addHeader (String name, String value) {}
+        @Override public void setIntHeader (String name, int value) {}
+        @Override public void addIntHeader (String name, int value) {}
+        @Override public void setStatus (int sc) {}
+        @Deprecated @Override public void setStatus (int sc, String sm) {}
+        @Override public int getStatus () { return 0; }
+        @Override public String getHeader (String name) { return null; }
+        @Override public Collection<String> getHeaders (String name) { return null; }
+        @Override public Collection<String> getHeaderNames () { return null; }
+        @Override public String getCharacterEncoding () { return null; }
+        @Override public String getContentType () { return null; }
+        @Override public ServletOutputStream getOutputStream () throws IOException { return null; }
+        @Override public PrintWriter getWriter () throws IOException { return null; }
+        @Override public void setCharacterEncoding (String charset) {}
+        @Override public void setContentLength (int len) {}
+        @Override public void setContentLengthLong (long len) {}
+        @Override public void setContentType (String type) {}
+        @Override public void setBufferSize (int size) {}
+        @Override public int getBufferSize () { return 0; }
+        @Override public void flushBuffer () throws IOException {}
+        @Override public void resetBuffer () {}
+        @Override public boolean isCommitted () { return false; }
+        @Override public void reset () {}
+        @Override public void setLocale (Locale loc) {}
+        @Override public Locale getLocale () { return null; }
+    }
+
+    private Request createRequest () {
+        RouteMatch match = new RouteMatch (null, "/", "/");
+        MockedHttpServletRequest servletRequest = new MockedHttpServletRequest (null);
+        MockedHttpServletResponse servletResponse = new MockedHttpServletResponse ();
+        return new Request (match, servletRequest, servletResponse);
     }
 }
