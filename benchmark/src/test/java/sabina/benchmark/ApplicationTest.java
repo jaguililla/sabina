8c84c0d76887d8d35d415a7e6e14735bc7f81f30
jamming <juanjoaguililla@gmail.com>
2015-03-25 11:38:28 +0100

    refactor(benchmark): parameter changes
    * Change stress tests executions to speed up the build process
    * Use MongoDB persistence in tests for the same reason

--- benchmark/src/test/java/sabina/benchmark/ApplicationTest.java
+++ benchmark/src/test/java/sabina/benchmark/ApplicationTest.java
@@ -38,7 +38,7 @@ import org.testng.annotations.Test;
  * <p>TODO Change assert's order
  */
 public final class ApplicationTest {
-    private static final int THREADS = 1, EXECUTIONS = 1, WARM_UP = 0;
+    private static final int THREADS = 8, EXECUTIONS = 75, WARM_UP = 5;
 
     private static final String ENDPOINT = "http://localhost:5050";
     private static final Gson GSON = new Gson ();
