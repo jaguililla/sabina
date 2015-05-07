c49374974d4251945669b1188a0ffd885838fea1
jamming <juanjoaguililla@gmail.com>
2015-05-07 20:58:56 +0200

    refactor(blog): reformat code and change to Java 8

--- blog/src/main/java/course/BlogPostDAO.java
+++ blog/src/main/java/course/BlogPostDAO.java
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -17,88 +17,79 @@
 
 package course;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.DBObject;
-import com.mongodb.WriteResult;
-
 import java.util.List;
+
+import com.mongodb.*;
 
 public class BlogPostDAO {
     DBCollection postsCollection;
 
-    public BlogPostDAO(final DB blogDatabase) {
-        postsCollection = blogDatabase.getCollection("posts");
+    public BlogPostDAO (final DB blogDatabase) {
+        postsCollection = blogDatabase.getCollection ("posts");
     }
 
-    public DBObject findByPermalink(String permalink) {
-        DBObject post = postsCollection.findOne(new BasicDBObject("permalink", permalink));
-
-
-        return post;
+    public DBObject findByPermalink (String permalink) {
+        return postsCollection.findOne (new BasicDBObject ("permalink", permalink));
     }
 
-    public List<DBObject> findByDateDescending(int limit) {
-        List<DBObject> posts;
-        DBCursor cursor = postsCollection.find().sort(new BasicDBObject().append("date", -1)).limit(limit);
-        try {
-            posts = cursor.toArray();
-        } finally {
-            cursor.close();
+    public List<DBObject> findByDateDescending (int limit) {
+        try (DBCursor cursor = postsCollection.find ()) {
+            return cursor
+                .sort (new BasicDBObject ("date", -1))
+                .limit (limit).toArray ();
         }
-        return posts;
     }
 
-    public List<DBObject> findByTagDateDescending(final String tag) {
-        List<DBObject> posts;
-        BasicDBObject query = new BasicDBObject("tags", tag);
-        System.out.println("/tag query: " + query.toString());
-        DBCursor cursor = postsCollection.find(query).sort(new BasicDBObject().append("date", -1)).limit(10);
-        try {
-            posts = cursor.toArray();
-        } finally {
-            cursor.close();
+    public List<DBObject> findByTagDateDescending (final String tag) {
+        BasicDBObject query = new BasicDBObject ("tags", tag);
+        System.out.println ("/tag query: " + query.toString ());
+
+        try (DBCursor cursor = postsCollection.find (query)) {
+            return cursor
+                .sort (new BasicDBObject ("date", -1))
+                .limit (10)
+                .toArray ();
         }
-        return posts;
     }
 
-    public String addPost(String title, String body, List tags, String username) {
+    public String addPost (String title, String body, List tags, String username) {
+        System.out.println ("inserting blog entry " + title + " " + body);
 
-        System.out.println("inserting blog entry " + title + " " + body);
+        String permalink = title.replaceAll ("\\s", "_"); // whitespace becomes _
+        permalink = permalink.replaceAll ("\\W", ""); // get rid of non alphanumeric
+        permalink = permalink.toLowerCase ();
 
-        String permalink = title.replaceAll("\\s", "_"); // whitespace becomes _
-        permalink = permalink.replaceAll("\\W", ""); // get rid of non alphanumeric
-        permalink = permalink.toLowerCase();
-
-        BasicDBObject post = new BasicDBObject("title", title);
-        post.append("author", username);
-        post.append("body", body);
-        post.append("permalink", permalink);
-        post.append("tags", tags);
-        post.append("comments", new java.util.ArrayList());
-        post.append("date", new java.util.Date());
+        BasicDBObject post = new BasicDBObject ("title", title);
+        post.append ("author", username);
+        post.append ("body", body);
+        post.append ("permalink", permalink);
+        post.append ("tags", tags);
+        post.append ("comments", new java.util.ArrayList ());
+        post.append ("date", new java.util.Date ());
 
         try {
-            postsCollection.insert(post);
-            System.out.println("Inserting blog post with permalink " + permalink);
-        } catch (Exception e) {
-            System.out.println("Error inserting post");
+            postsCollection.insert (post);
+            System.out.println ("Inserting blog post with permalink " + permalink);
+        }
+        catch (Exception e) {
+            System.out.println ("Error inserting post");
             return null;
         }
 
         return permalink;
     }
 
-    public void addPostComment(final String name, final String email, final String body, final String permalink) {
-        BasicDBObject comment = new BasicDBObject("author", name).append("body", body);
-        if (email != null && !email.equals("")) {
-            comment.append("email", email);
+    public void addPostComment (
+        final String name, final String email, final String body, final String permalink) {
+
+        BasicDBObject comment = new BasicDBObject ("author", name).append ("body", body);
+        if (email != null && !email.equals ("")) {
+            comment.append ("email", email);
         }
 
-        WriteResult result = postsCollection.update(new BasicDBObject("permalink", permalink),
-                new BasicDBObject("$push", new BasicDBObject("comments", comment)), false, false);
+        postsCollection.update (
+            new BasicDBObject ("permalink", permalink),
+            new BasicDBObject ("$push", new BasicDBObject ("comments", comment)),
+            false, false);
     }
-
 }
--- blog/src/main/java/course/SessionDAO.java
+++ blog/src/main/java/course/SessionDAO.java
@@ -4,7 +4,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -17,63 +17,56 @@
 
 package course;
 
+import java.security.SecureRandom;
+
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 import sun.misc.BASE64Encoder;
 
-import java.security.SecureRandom;
-
 public class SessionDAO {
     private final DBCollection sessionsCollection;
 
-    public SessionDAO(final DB blogDatabase) {
-        sessionsCollection = blogDatabase.getCollection("sessions");
+    public SessionDAO (final DB blogDatabase) {
+        sessionsCollection = blogDatabase.getCollection ("sessions");
     }
 
+    public String findUserNameBySessionId (String sessionId) {
+        DBObject session = getSession (sessionId);
 
-    public String findUserNameBySessionId(String sessionId) {
-        DBObject session = getSession(sessionId);
-
-        if (session == null) {
-            return null;
-        }
-        else {
-            return session.get("username").toString();
-        }
-    }
-
-
-    // starts a new session in the sessions table
-    public String startSession(String username) {
-
-        // get 32 byte random number. that's a lot of bits.
-        SecureRandom generator = new SecureRandom();
-        byte randomBytes[] = new byte[32];
-        generator.nextBytes(randomBytes);
-
-        BASE64Encoder encoder = new BASE64Encoder();
-
-        String sessionID = encoder.encode(randomBytes);
-
-        // build the BSON object
-        BasicDBObject session = new BasicDBObject("username", username);
-
-        session.append("_id", sessionID);
-
-        sessionsCollection.insert(session);
-
-        return session.getString("_id");
-    }
-
-    // ends the session by deleting it from the sesisons table
-    public void endSession(String sessionID) {
-        sessionsCollection.remove(new BasicDBObject("_id", sessionID));
+        return session == null? null : session.get ("username").toString ();
     }
 
     // retrieves the session from the sessions table
-    public DBObject getSession(String sessionID) {
-        return sessionsCollection.findOne(new BasicDBObject("_id", sessionID));
+    public DBObject getSession (String sessionID) {
+        return sessionsCollection.findOne (new BasicDBObject ("_id", sessionID));
+    }
+
+    // starts a new session in the sessions table
+    public String startSession (String username) {
+
+        // get 32 byte random number. that's a lot of bits.
+        SecureRandom generator = new SecureRandom ();
+        byte randomBytes[] = new byte[32];
+        generator.nextBytes (randomBytes);
+
+        BASE64Encoder encoder = new BASE64Encoder ();
+
+        String sessionID = encoder.encode (randomBytes);
+
+        // build the BSON object
+        BasicDBObject session = new BasicDBObject ("username", username);
+
+        session.append ("_id", sessionID);
+
+        sessionsCollection.insert (session);
+
+        return session.getString ("_id");
+    }
+
+    // ends the session by deleting it from the sesisons table
+    public void endSession (String sessionID) {
+        sessionsCollection.remove (new BasicDBObject ("_id", sessionID));
     }
 }
