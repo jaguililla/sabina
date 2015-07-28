0e8f20a58e2108a8a3c932efeafeecf428858f03
jamming <juanjoaguililla@gmail.com>
2015-07-29 00:51:20 +0200

    Add Groovy support for tests

---
+++ http/src/test/groovy/sabina/util/StringsTest.groovy
@@ -0,0 +1,17 @@
+package sabina.util
+
+import org.testng.annotations.Test
+
+@Test
+public class StringsTest {
+    public void """
+        filter ()
+
+        Given a string with no variables
+        And any non null map of variables
+        When the string is filtered
+        Then the result is the same string
+    """ () {
+
+    }
+}
