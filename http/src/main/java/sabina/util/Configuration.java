42e2b76ffa86287459f1210985e6e73c68f37c2e
jamming <juanjoaguililla@gmail.com>
2015-11-11 23:25:31 +0100

    Simplify configuration utility

--- http/src/main/java/sabina/util/Configuration.java
+++ http/src/main/java/sabina/util/Configuration.java
@@ -3,11 +3,8 @@ package sabina.util;
 import static java.lang.Long.parseLong;
 import static java.lang.System.getProperties;
 import static java.util.Arrays.stream;
-import static java.util.regex.Pattern.CASE_INSENSITIVE;
-import static java.util.regex.Pattern.compile;
 import static java.util.stream.Collectors.toMap;
 import static sabina.util.Builders.entry;
-import static sabina.util.Builders.tmap;
 import static sabina.util.Checks.checkArgument;
 import static sabina.util.Strings.isEmpty;
 
@@ -18,8 +15,6 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.*;
 import java.util.Map.Entry;
-import java.util.function.Function;
-import java.util.regex.Pattern;
 
 /**
  * Read only settings. It is not allowed to store or set parameters, reloading is allowed and
@@ -60,58 +55,6 @@ public final class Configuration {
     private static Configuration instance;
 
     /**
-     _		No
-     _1		No
-     1_		No
-     _1_	No
-     1		Yes
-     1_0	Yes
-     1__0	Yes
-     1_0_2	Yes
-     1-		No
-     -1		Yes
-     a		No
-     0a		No
-     */
-    private static final String DEC_RE = "-?\\d+(_*\\d)*";
-
-    /**
-     0x_		No
-     0x_1		No
-     0x1_		No
-     0x_1_		No
-     0x1		Yes
-     0x1_0		Yes
-     0x1__0		Yes
-     0x1_0_2	Yes
-     0x1-		No
-     -0x1		Yes
-     0xa		Yes
-     0x0a		Yes
-     0xg		No
-     0x0g		No
-     */
-    private static final String HEX_RE = "-?0x\\p{XDigit}+(_*\\p{XDigit})*";
-
-    /**
-     *
-     */
-    private static final String OCT_RE = "-?0[0-7]+(_*[0-7])*";
-
-    private static final String FLOAT_RE = "\\d+(_*\\d)*";
-
-    private static final Map<Pattern, Function<String, Object>> converters = tmap (
-        entry (compileCi (DEC_RE + "y"), v -> Byte.decode (v.substring (v.length () - 1))),
-        entry (compileCi ("true|false"), Boolean::valueOf),
-        entry (compile ("'.'"), v -> v.charAt (1)), // Unicode is translated by properties
-        entry (compile ("\".*\""), v -> v.substring (1, v.length () - 1))
-    );
-
-    static final Pattern compileCi (String pattern) {
-        return compile (pattern, CASE_INSENSITIVE);
-    }
-
-    /**
      * Gets the configuration instance.
      *
      * @return The unique Configuration instance.
@@ -120,7 +63,7 @@ public final class Configuration {
         return instance == null? instance = new Configuration () : instance;
     }
 
-    public static Map<String, String> url (String inputs) {
+    public static Map<String, Object> url (String inputs) {
         try {
             return loadStream (new URL (inputs).openConnection ().getInputStream ());
         }
@@ -129,11 +72,11 @@ public final class Configuration {
         }
     }
 
-    public static Map<String, String> resource (String inputs) {
+    public static Map<String, Object> resource (String inputs) {
         return loadStream (Class.class.getResourceAsStream (inputs));
     }
 
-    public static Map<String, String> file (String inputs) {
+    public static Map<String, Object> file (String inputs) {
         try {
             return loadStream (new FileInputStream (inputs));
         }
@@ -142,7 +85,7 @@ public final class Configuration {
         }
     }
 
-    public static Map<String, String> system (String prefix) {
+    public static Map<String, Object> system (String prefix) {
         checkArgument (!isEmpty (prefix));
 
         return getProperties ().keySet ().stream ()
@@ -159,10 +102,10 @@ public final class Configuration {
      * @param inputs .
      * @return .
      */
-    public static Map<String, String> parameters (String[] inputs) {
+    public static Map<String, Object> parameters (String[] inputs) {
         checkArgument (inputs != null && inputs.length % 2 == 0);
 
-        Map<String, String> result = new HashMap<> ();
+        Map<String, Object> result = new HashMap<> ();
 
         for (int ii = 0; ii < inputs.length; ii += 2) {
             String odd = inputs[ii].trim ();
@@ -180,7 +123,7 @@ public final class Configuration {
         return result;
     }
 
-    static Map<String, String> loadStream (InputStream stream) {
+    static Map<String, Object> loadStream (InputStream stream) {
         if (stream == null)
             return new HashMap<> ();
 
@@ -218,16 +161,11 @@ public final class Configuration {
      *
      * @param entries .
      */
-    @SafeVarargs public final Configuration load (Map<String, String>... entries) {
+    @SafeVarargs public final Configuration load (Map<String, Object>... entries) {
         stream (entries).forEach (settings::putAll);
         settings = settings.entrySet ().stream ()
-            .map (this::setType)
             .collect (toMap (Entry::getKey, Entry::getValue));
         return this;
-    }
-
-    private Entry<String, Object> setType (Entry<String, Object> entry) {
-        return entry; // TODO Convert value to the right type (using scanner?)
     }
 
     public Set<String> keys () {
